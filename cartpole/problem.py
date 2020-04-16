448f16c5de138b8e99fdd2b6e62f354fae76fb49
Abhishek Cauligi <acauligi@stanford.edu>
2020-04-16 14:38:29 -0700

    Creating class structures for opt

--- cartpole/problem.py
+++ cartpole/problem.py
@@ -1,130 +1,118 @@
+import mosek
 import cvxpy as cp
 import numpy as np
-import mosek
 
-n,m = 4,3
-N = 11 
-dh = 0.1
-n_obs = 8
+import sys
+import pdb
+import h5py
 
-x0 = np.array([-0.39844222352768077, 0.20543510691704736, 0.24126227864050964, 0.4491739326585802])
-xg = np.zeros(n)
+class Optimizer():
+  def __init__(self, idx):
+    fn = 'data/testdata{}.h5'.format(idx+1)
+    f = h5py.File(fn, 'r')
 
-Q = np.diag(np.array([10.,10,10,10]))
-R = np.diag(np.array([1.,0,0]))
+    self.X = f['X'][()].T
+    self.U = f['U'][()].T
+    self.Y = f['Y'][()].T
 
-l = 1.
-mc = 1.
-mp = 1.
-g = 10.
-dh = 0.05
-kappa,nu = 100., 30.
+    self.N = f['N'][()]
+    self.n = int(self.X.shape[0])
+    self.m = int(self.U.shape[0])
+    self.n_probs = self.X.shape[-1]
 
-# state bounds
-dist = 0.5
-x_max = np.array([dist, np.pi/8, 2, 1])
-x_min = -x_max
+    self.X0 = f['X0'][()].T
+    self.Xg = f['Xg'][()].T
+    self.solve_time = f['solve_time'][()]
+    self.J = f['J'][()]
+    self.node_count = f['node_count'][()]
 
-delta_min = np.array([
-    -x_max[0] + l*x_min[1] - dist,
-    x_min[0] - l*x_max[1] - dist
-])
-ddelta_min = np.array([
-    -x_max[2] + l*x_min[3],
-    x_min[2] - l*x_max[3]
-])
+    self.Ak = f['Ak'][()].T
+    self.Bk = f['Bk'][()].T
+    self.Q = f['Q'][()]
+    self.R = f['R'][()]
+    self.x_min = f['x_min'][()]
+    self.x_max = f['x_max'][()]
+    self.uc_min = f['uc_min'][()]
+    self.uc_max = f['uc_max'][()]
+    self.sc_min = f['sc_min'][()]
+    self.sc_max = f['sc_max'][()]
+    self.delta_min = f['delta_min'][()]
+    self.delta_max = f['delta_min'][()]
+    self.ddelta_min = f['ddelta_min'][()]
+    self.ddelta_max = f['ddelta_max'][()]
+    self.dh = f['dh'][()]
+    self.g = f['g'][()]
+    self.l = f['l'][()]
+    self.mc = f['mc'][()]
+    self.mp = f['mp'][()]
+    self.kappa = f['kappa'][()]
+    self.nu = f['nu'][()]
+    self.dist = f['dist'][()]
 
-delta_max = np.array([
-    -x_min[0] + l*x_max[1] - dist,
-    x_max[0] - l*x_min[1] - dist
-])
-ddelta_max = np.array([
-    -x_min[2] + l*x_max[3],
-    x_max[2] - l*x_min[3]
-])
+  def construct_problem(self,prob_idx):
+    cons = []
 
-# control bounds
-uc_min = -2.
-uc_max = -uc_min
-sc_min = kappa*delta_min + nu*ddelta_min
-sc_max = kappa*delta_max + nu*ddelta_max
+    x = cp.Variable((self.n,self.N))
+    u = cp.Variable((self.m, self.N-1))
+    sc = u[1:,:]
+    y = cp.Variable((4, self.N-1), boolean=True)
 
-# dynamics
-A = np.vstack((
-    np.hstack((np.zeros((2,2)), np.eye(2))),
-    np.zeros((2,4))
-))
-A[2,1] = g*mp/mc
-A[3,1] = g*(mc+mp)/(l*mc)
-B = np.zeros((n,m))
-B[2,0] = 1/mc
-B[3,:] = np.array([1/(l*mc), -1/(l*mp), 1/(l*mp)])
-Ak = np.eye(n) + dh*A
-Bk = dh*B
+    # Initial condition
+    cons += [x[:,0] == self.X0[:,prob_idx]]
 
-# Decision variables
-cons = []
+    # Dynamics constraints
+    for kk in range(self.N-1):
+      cons += [x[:,kk+1] - (self.Ak @ x[:,kk] + self.Bk @ u[:,kk]) == np.zeros(self.n)]
 
-x = cp.Variable((n,N))
-u = cp.Variable((m, N-1))
-y = cp.Variable((4, N-1), integer=True)
+    # State and control constraints
+    for kk in range(self.N):
+      cons += [self.x_min - x[:,kk] <= np.zeros(self.n)] 
+      cons += [x[:,kk] - self.x_max <= np.zeros(self.n)]
 
-# Initial condition
-cons +=  [x[:,0] == x0]
+    for kk in range(self.N-1):
+      cons += [self.uc_min - u[0,kk] <= 0.]
+      cons += [u[0,kk] - self.uc_max <= 0]
 
-# Dynamics constraints
-for kk in range(N-1):
-  cons += [x[:,kk+1] - (Ak @ x[:,kk] + Bk @ u[:,kk]) == np.zeros(n)]
+    # Binary variable constraints
+    for kk in range(self.N-1):
+      for jj in range(2):
+        if jj == 0:
+          d_k    = -x[0,kk] + self.l*x[1,kk] - self.dist
+          dd_k   = -x[2,kk] + self.l*x[3,kk]
+        else:
+          d_k    =  x[0,kk] - self.l*x[1,kk] - self.dist
+          dd_k   =  x[2,kk] - self.l*x[3,kk]
 
-# State and control constraints
-for kk in range(N):
-  cons += [x_min - x[:,kk] <= np.zeros(n)] 
-  cons += [x[:,kk] - x_max <= np.zeros(n)]
+        y_l, y_r = y[2*jj:2*jj+2,kk]
+        d_min, d_max = self.delta_min[jj], self.delta_max[jj]
+        dd_min, dd_max = self.ddelta_min[jj], self.ddelta_max[jj]
+        f_min, f_max = self.sc_min[jj], self.sc_max[jj]
 
-for kk in range(N-1):
-  cons += [uc_min - u[0,kk] <= 0.]
-  cons += [u[0,kk] - uc_max <= 0]
+        # Eq. (26a)
+        cons += [d_min*(1-y_l) <= d_k]
+        cons += [d_k <= d_max*y_l]
 
-# Binary variable constraints
-for kk in range(N-1):
-  for jj in range(2):
-    if jj == 0:
-      d_k    = -x[0,kk] + l*x[1,kk] - dist
-      dd_k   = -x[2,kk] + l*x[3,kk]
-    else:
-      d_k    =  x[0,kk] - l*x[1,kk] - dist
-      dd_k   =  x[2,kk] - l*x[3,kk]
+        # Eq. (26b)
+        cons += [f_min*(1-y_r) <= self.kappa*d_k + self.nu*dd_k]
+        cons += [self.kappa*d_k + self.nu*dd_k <= f_max*y_r]
 
-    y_l = y[2*jj,kk]
-    y_r = y[2*jj+1,kk]    
-    d_min,d_max = delta_min[jj],delta_max[jj]
-    dd_min,dd_max = ddelta_min[jj],ddelta_max[jj]
-    f_min,f_max = sc_min[jj],sc_max[jj]
+        # Eq. (27)
+        cons += [self.nu*dd_max*(y_l-1) <= sc[jj,kk] - self.kappa*d_k - self.nu*dd_k]
+        cons += [sc[jj,kk] - self.kappa*d_k - self.nu*dd_k <= f_min*(y_r-1)]
 
-    # Eq. (26a)
-    cons += [d_min*(1-y_l) <= d_k]
-    cons += [d_k <= d_max*y_l]
+        cons += [sc[jj,kk] <= 0]
+        cons += [sc[jj,kk] <= f_max*y_l]
+        cons += [sc[jj,kk] <= f_max*y_r]
 
-    # Eq. (26b)
-    cons += [f_min*(1-y_r) <= kappa*d_k + nu*dd_k]
-    cons += [kappa*d_k + nu*dd_k <= f_max*y_r]
+    # LQR cost
+    lqr_cost = 0.
+    for kk in range(self.N):
+      lqr_cost += cp.quad_form(x[:,kk]-self.Xg[:,prob_idx], self.Q)
+    for kk in range(self.N-1):
+      lqr_cost += cp.quad_form(u[:,kk],self.R)
 
-    # Eq. (27)
-    cons += [nu*dd_max*(y_l-1) <= u[jj+1,kk] - kappa*d_k-nu*dd_k]
-    cons += [u[jj+1,kk] - kappa*d_k-nu*dd_k <= f_min*(y_r-1)]
+    self.prob = cp.Problem(cp.Minimize(lqr_cost), cons)
+    return x, u, y
 
-    cons += [-u[jj+1,kk] <= 0]
-    cons += [u[jj+1,kk] <= f_max*y_l]
-    cons += [u[jj+1,kk] <= f_max*y_r]
-
-# LQR cost
-lqr_cost = 0.
-for kk in range(N):
-  lqr_cost += cp.quad_form(x[:,kk]-xg, Q)
-for kk in range(N-1):
-  lqr_cost += cp.quad_form(u[:,kk],R)
-
-prob = cp.Problem(cp.Minimize(lqr_cost), cons)
-prob.solve()
-
-# return prob, x, u, y 
+  def solve(self):
+    self.prob.solve(solver=cp.MOSEK)
