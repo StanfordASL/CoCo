9a534f012f154eecbcbc1b7043b6ec60ab8a399b
Abhishek Cauligi <acauligi@stanford.edu>
2021-06-06 18:40:33 -0700

    Saving state for MPC trajectory generation

--- free_flyer/free_flyer.py
+++ free_flyer/free_flyer.py
@@ -69,48 +69,50 @@ class FreeFlyer(Problem):
 
         # Dynamics constraints
         for ii in range(self.N-1):
-          cons += [x[:,ii+1] - (self.Ak @ x[:,ii] + self.Bk @ u[:,ii]) == np.zeros(2*self.n)]
+            cons += [x[:,ii+1] - (self.Ak @ x[:,ii] + self.Bk @ u[:,ii]) == np.zeros(2*self.n)]
 
         M = 100. # big M value
         for i_obs in range(self.n_obs):
-          for i_dim in range(self.n):
-            o_min = obstacles[self.n*i_dim,i_obs]
-            o_max = obstacles[self.n*i_dim+1,i_obs]
+            for i_dim in range(self.n):
+                o_min = obstacles[self.n*i_dim,i_obs]
+                o_max = obstacles[self.n*i_dim+1,i_obs]
+
+                for i_t in range(self.N-1):
+                    yvar_min = 4*i_obs + self.n*i_dim
+                    yvar_max = 4*i_obs + self.n*i_dim + 1
+
+                    cons += [x[i_dim,i_t+1] <= o_min + M*y[yvar_min,i_t]]
+                    cons += [-x[i_dim,i_t+1] <= -o_max + M*y[yvar_max,i_t]]
 
             for i_t in range(self.N-1):
-              yvar_min = 4*i_obs + self.n*i_dim
-              yvar_max = 4*i_obs + self.n*i_dim + 1
-
-              cons += [x[i_dim,i_t+1] <= o_min + M*y[yvar_min,i_t]]
-              cons += [-x[i_dim,i_t+1] <= -o_max + M*y[yvar_max,i_t]]
-
-          for i_t in range(self.N-1):
-            yvar_min, yvar_max = 4*i_obs, 4*(i_obs+1)
-            cons += [sum([y[ii,i_t] for ii in range(yvar_min,yvar_max)]) <= 3]
+                yvar_min, yvar_max = 4*i_obs, 4*(i_obs+1)
+                cons += [sum([y[ii,i_t] for ii in range(yvar_min,yvar_max)]) <= 3]
 
         # Region bounds
         for kk in range(self.N):
-          for jj in range(self.n):
-            cons += [self.posmin[jj] - x[jj,kk] <= 0]
-            cons += [x[jj,kk] - self.posmax[jj] <= 0]
+            for jj in range(self.n):
+                cons += [self.posmin[jj] - x[jj,kk] <= 0]
+                cons += [x[jj,kk] - self.posmax[jj] <= 0]
 
         # Velocity constraints
         for kk in range(self.N):
-          for jj in range(self.n):
-            cons += [self.velmin - x[self.n+jj,kk] <= 0]
-            cons += [x[self.n+jj,kk] - self.velmax <= 0]
+            for jj in range(self.n):
+                cons += [self.velmin - x[self.n+jj,kk] <= 0]
+                cons += [x[self.n+jj,kk] - self.velmax <= 0]
 
         # Control constraints
         for kk in range(self.N-1):
-          cons += [cp.norm(u[:,kk]) <= self.umax]
+            for jj in range(self.m):
+                cons += [self.umin - u[jj,kk] <= 0]
+                cons += [u[jj,kk] - self.umax <= 0]
 
         lqr_cost = 0.
         # l2-norm of lqr_cost
         for kk in range(self.N):
-          lqr_cost += cp.quad_form(x[:,kk]-xg, self.Q)
+            lqr_cost += cp.quad_form(x[:,kk]-xg, self.Q)
 
         for kk in range(self.N-1):
-          lqr_cost += cp.quad_form(u[:,kk], self.R)
+            lqr_cost += cp.quad_form(u[:,kk], self.R)
 
         self.bin_prob = cp.Problem(cp.Minimize(lqr_cost), cons)
 
